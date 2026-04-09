e16959c3d7b4f83d39b2d1ed43c550f7464738c0
mreichelt <m.reichelt@hotmail.com>
2026-04-09 14:07:20 +0200

    also derivative matrices seem to work

--- python/hmod/hilbert_matrices.py
+++ python/hmod/hilbert_matrices.py
@@ -3,7 +3,7 @@ from scipy.sparse.linalg import LinearOperator
 import numpy as np
 import hmod.block_operations as block_ops
 import hmod.standard_matrices as sm
-
+import hmod.polynomial_bases as pb
 
 class DST_IV(LinearOperator):
     def __init__(self, nt: int, workers=-1):
@@ -17,7 +17,7 @@ class DST_IV(LinearOperator):
         return dst(x, type=4, axis=0, workers=self.workers)
 
     def _rmatmat(self, x):
-        return self._matmat(x)  #dst-iv is self-adjoint
+        return self._matmat(x)  # dst-iv is self-adjoint
 
 
 class DCT_IV(LinearOperator):
@@ -32,7 +32,7 @@ class DCT_IV(LinearOperator):
         return dct(x, type=4, axis=0, workers=self.workers)
 
     def _rmatmat(self, x):
-        return self._matmat(x)  #dct-iv is self-adjoint
+        return self._matmat(x)  # dct-iv is self-adjoint
 
 
 def get_trial_transform(pol_deg, nt, workers=-1):
@@ -50,6 +50,7 @@ def get_trial_transform(pol_deg, nt, workers=-1):
     block_operator = block_ops.BlockLinearOperator(blocks)
     return block_operator
 
+
 def get_test_transform(pol_deg, nt, workers=-1):
     from hmod.block_operations import BlockLinearOperator
     blocks = [[None for _ in range(pol_deg + 1)] for _ in range(pol_deg + 1)]
@@ -65,82 +66,103 @@ def get_test_transform(pol_deg, nt, workers=-1):
     block_operator = block_ops.BlockLinearOperator(blocks)
     return block_operator
 
-def first_branch_sum(diagonal_index : int, nt : int, pol_deg_trial : int, pol_deg_test : int, workers : int = -1):
+
+def first_branch_sum(diagonal_index: int, nt: int, pol_deg_trial: int, pol_deg_test: int, workers: int = -1):
     assert 0 <= diagonal_index < nt, "Diagonal index must be between 0 and nt-1"
     from scipy.special import spherical_jn as jn
-    alpha_k = lambda k : np.pi*(2*k+1)/(4*nt)
-    alpha_q = lambda q : alpha_k(2*q*nt+diagonal_index)
+    alpha_k = lambda k: np.pi * (2 * k + 1) / (4 * nt)
+    alpha_q = lambda q: alpha_k(2 * q * nt + diagonal_index)
     nmodes = int(1e5)
-    summand = lambda q : jn(pol_deg_test, alpha_q(q))*jn(pol_deg_trial, alpha_q(q))
-    #sum in reversed order
+    summand = lambda q: jn(pol_deg_test, alpha_q(q)) * jn(pol_deg_trial, alpha_q(q))
+    # sum in reversed order
     q_vals = np.arange(nmodes)[::-1]
-    return np.sum(summand(q_vals))/(nt**2)
+    return np.sum(summand(q_vals)) / (nt ** 2)
 
-def second_branch_sum(diagonal_index : int, nt : int, pol_deg_trial : int, pol_deg_test : int, workers : int = -1):
+
+def second_branch_sum(diagonal_index: int, nt: int, pol_deg_trial: int, pol_deg_test: int, workers: int = -1):
     assert 0 <= diagonal_index < nt, "Diagonal index must be between 0 and nt-1"
     from scipy.special import spherical_jn as jn
-    alpha_k = lambda k : np.pi*(2*k+1)/(4*nt)
-    alpha_q = lambda q : alpha_k(2*nt*(q+1)-1-diagonal_index)
+    alpha_k = lambda k: np.pi * (2 * k + 1) / (4 * nt)
+    alpha_q = lambda q: alpha_k(2 * nt * (q + 1) - 1 - diagonal_index)
     nmodes = int(1e5)
-    summand = lambda q : jn(pol_deg_test, alpha_q(q))*jn(pol_deg_trial, alpha_q(q))
-    #sum in reversed order
+    summand = lambda q: jn(pol_deg_test, alpha_q(q)) * jn(pol_deg_trial, alpha_q(q))
+    # sum in reversed order
     q_vals = np.arange(nmodes)[::-1]
-    return np.sum(summand(q_vals))/(nt**2)
+    return np.sum(summand(q_vals)) / (nt ** 2)
 
 
-def get_kernel_matrix_for_degrees(nt : int, pol_deg_trial : int, pol_deg_test : int):
+def get_kernel_matrix_for_degrees(nt: int, pol_deg_trial: int, pol_deg_test: int):
     fac = -1.
     if pol_deg_trial % 2 != pol_deg_test % 2:
         fac = 1.
-    entry_i = lambda i : first_branch_sum(i, nt, pol_deg_trial, pol_deg_test) + fac*second_branch_sum(i, nt, pol_deg_trial, pol_deg_test)
+    entry_i = lambda i: first_branch_sum(i, nt, pol_deg_trial, pol_deg_test) + fac * second_branch_sum(i, nt,
+                                                                                                       pol_deg_trial,
+                                                                                                       pol_deg_test)
     return sparse.diags([entry_i(i) for i in range(nt)], format='csr')
 
 
-def get_kernel_matrix(nt : int, pol_deg_trial : int, pol_deg_test : int):
-    blocks = [[get_kernel_matrix_for_degrees(nt, n, m) for n in range(pol_deg_trial + 1)] for m in range(pol_deg_test + 1)]
+def get_kernel_matrix(nt: int, pol_deg_trial: int, pol_deg_test: int):
+    blocks = [[get_kernel_matrix_for_degrees(nt, n, m) for n in range(pol_deg_trial + 1)] for m in
+              range(pol_deg_test + 1)]
     return sparse.bmat(blocks, format='csr')
 
 
-
-def get_operator_I_H_legendre_legendre(nt : int, pol_deg_trial : int, pol_deg_test : int, final_time :float):
+def get_operator_I_H_legendre_legendre(nt: int, pol_deg_trial: int, pol_deg_test: int, final_time: float):
     nmodes = int(1e5)
     fourier_facs = np.ones(nmodes)
     U = get_trial_transform(pol_deg_trial, nt)
     T = get_test_transform(pol_deg_test, nt)
     K = sparse.linalg.aslinearoperator(get_kernel_matrix(nt, pol_deg_trial, pol_deg_test))
-    return (final_time*0.5)*T.H @ K @ U
+    return (final_time * 0.5) * T.H @ K @ U
 
-def get_hilbert_matrix_with_derivatives_legendre_legendre(nt : int, pol_deg_trial : int, pol_deg_test : int,
-                                                          derivatives_trial : int, derivatives_test : int, final_time :float):
+
+def get_hilbert_matrix_with_derivatives_legendre_legendre(nt: int, pol_deg_trial: int, pol_deg_test: int,
+                                                          derivatives_trial: int, derivatives_test: int,
+                                                          final_time: float):
     Mh = get_operator_I_H_legendre_legendre(nt, pol_deg_trial, pol_deg_test, final_time)
-    if derivatives_trial >0 or derivatives_test > 0:
-        raise NotImplementedError("Derivatives not implemented yet")
+    Mat = Mh
+    if derivatives_trial > 0:
+        D_trial = pb.get_legendre_derivative_matrix(nt, pol_deg_trial, final_time)
+        D_pow = sparse.linalg.matrix_power(D_trial, derivatives_trial)
+        D_op = sparse.linalg.aslinearoperator(D_pow)
+        Mat = Mat @ D_op
+    if derivatives_test > 0:
+        D_test = pb.get_legendre_derivative_matrix(nt, pol_deg_test, final_time)
+        D_pow = sparse.linalg.matrix_power(D_test, derivatives_test)
+        D_op = sparse.linalg.aslinearoperator(D_pow)
+        Mat = D_op.H @ Mat
 
-    return Mh
+    return Mat
 
-def get_hilbert_matrix_with_derivatives_legendre_lagrange(nt : int, pol_deg_trial : int, pol_deg_test : int,
-                                                          derivatives_trial : int, derivatives_test : int, final_time :float):
+
+def get_hilbert_matrix_with_derivatives_legendre_lagrange(nt: int, pol_deg_trial: int, pol_deg_test: int,
+                                                          derivatives_trial: int, derivatives_test: int,
+                                                          final_time: float):
     K = get_hilbert_matrix_with_derivatives_legendre_legendre(nt, pol_deg_trial
-                                                              , pol_deg_test, derivatives_trial, derivatives_test, final_time)
+                                                              , pol_deg_test, derivatives_trial, derivatives_test,
+                                                              final_time)
     trans = sparse.linalg.aslinearoperator(sm.get_lagrange_to_legendre_matrix(pol_deg_test, nt))
 
     return trans.T @ K
 
-def get_hilbert_matrix_with_derivatives_lagrange_legendre(nt : int, pol_deg_trial : int, pol_deg_test : int,
-                                                          derivatives_trial : int, derivatives_test : int, final_time :float):
+
+def get_hilbert_matrix_with_derivatives_lagrange_legendre(nt: int, pol_deg_trial: int, pol_deg_test: int,
+                                                          derivatives_trial: int, derivatives_test: int,
+                                                          final_time: float):
     K = get_hilbert_matrix_with_derivatives_legendre_legendre(nt, pol_deg_trial
-                                                              , pol_deg_test, derivatives_trial, derivatives_test, final_time)
+                                                              , pol_deg_test, derivatives_trial, derivatives_test,
+                                                              final_time)
     trans = sparse.linalg.aslinearoperator(sm.get_lagrange_to_legendre_matrix(pol_deg_test, nt))
 
     return K @ trans
 
-def get_hilbert_matrix_with_derivatives_lagrange_lagrange(nt : int, pol_deg_trial : int, pol_deg_test : int,
-                                                          derivatives_trial : int, derivatives_test : int, final_time :float):
+
+def get_hilbert_matrix_with_derivatives_lagrange_lagrange(nt: int, pol_deg_trial: int, pol_deg_test: int,
+                                                          derivatives_trial: int, derivatives_test: int,
+                                                          final_time: float):
     K = get_hilbert_matrix_with_derivatives_legendre_legendre(nt, pol_deg_trial
-                                                              , pol_deg_test, derivatives_trial, derivatives_test, final_time)
+                                                              , pol_deg_test, derivatives_trial, derivatives_test,
+                                                              final_time)
     trans = sparse.linalg.aslinearoperator(sm.get_lagrange_to_legendre_matrix(pol_deg_test, nt))
 
     return trans.T @ K @ trans
-
-
-
--- python/tests/test_hilbert_matrices.py
+++ python/tests/test_hilbert_matrices.py
@@ -72,3 +72,48 @@ def test_I_H_against_dense():
     diff_vec = I_H_dense - I_H_fft
     diff = np.linalg.norm(diff_vec)
     assert diff < 1e-4
+
+def test_rhs_against_dense():
+    import hilbertWrapper.hilbertWrapper as hw
+    f_analytic = lambda t: np.cos(2 * np.pi * t+5)
+    nt = 50
+    T = 1.0
+    tpoints = np.linspace(0, T, nt+1)
+    f_vec = 0.5 * (f_analytic(tpoints[:-1]) + f_analytic(tpoints[1:]))
+    polynomial_degree_test = 1
+    polynomial_degree_rhs = 0
+    n_modes = 100*nt
+    rhs_builder = hm.get_hilbert_matrix_with_derivatives_legendre_lagrange(nt, polynomial_degree_rhs, polynomial_degree_test, 0, 0, T)
+    rhs_fft = rhs_builder @ f_vec
+    #build the dense rhs matrix using hilbert wrapper
+    Fh = hw.get_rhs_mat(tpoints)
+    rhs_dense = Fh @ f_vec
+    diff_vec = rhs_dense - rhs_fft
+    diff = np.linalg.norm(diff_vec)
+    assert diff < 1e-4
+
+
+def test_dt_H_against_dense():
+    import hilbertWrapper.hilbertWrapper as hw
+    from hmod.matrix_tools import linear_operator_to_matrix
+    f_analytic = lambda t: np.cos(2 * np.pi * t+5)
+    nt = 5
+    T = 1.0
+    tpoints = np.linspace(0, T, nt+1)
+    polynomial_degree_trial = 1
+    polynomial_degree_test = 1
+    At_fft = hm.get_hilbert_matrix_with_derivatives_lagrange_lagrange(nt, polynomial_degree_trial, polynomial_degree_test, 1, 0, T)
+    Ath = hw.get_hilbert_stiffness_heat(tpoints)
+    #get a random vector
+    np.random.seed(0)
+    x = np.random.rand(nt*(polynomial_degree_trial)+1).astype(np.float64)
+    x[0] = 0.0  #enforce zero at t=0 as first column can be rubbish (either in old or new version)
+    dtH_fft = At_fft @ x
+    dtH_dense = Ath @ x
+    diff_vec = dtH_dense - dtH_fft
+    diff = np.linalg.norm(diff_vec)
+    assert diff < 1e-4
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
